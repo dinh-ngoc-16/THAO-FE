1f5bcf72b1cdf09e052b16f286429f06ec41e4d6
dnguyen <eonho6582@gmail.com>
2024-07-21 01:10:01 +0700

    fix: UI log in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,13 @@ import { Link, useNavigate } from "react-router-dom";
 import Input from "../components/Input";
 import { useState } from "react";
 import API from "../api";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 
 const Login = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({});
+  const [show, setShow] = useState(false);
 
   const handleOnChangeValue = (e, propertyName) => {
     setForm((prev) => ({ ...prev, [propertyName]: e.target.value }));
@@ -27,23 +30,48 @@ const Login = () => {
   return (
     <div className=" w-screen h-full flex flex-col justify-center items-center">
       <h1 className="mb-6">Login</h1>
-      <div className="input-group px-5 py-6 2xl:w-1/5 md:w-1/2 sm:w-full   border-2 bg-slate-200">
+      <div className="input-group px-5 py-6 2xl:w-1/5 md:w-1/2 sm:w-full border-2 bg-slate-200">
         <Input
           title="Username"
           onChange={(e) => handleOnChangeValue(e, "username")}
           maxLength={20}
         />
-        <Input
-          title="Password"
-          onChange={(e) => handleOnChangeValue(e, "pass")}
-          type="password"
-          maxLength={15}
-        />
+        <div className="relative">
+          <Input
+            title="Password"
+            onChange={(e) => handleOnChangeValue(e, "pass")}
+            type={show ? "text" : "password"}
+            maxLength={15}
+          />
+          <div
+            className="absolute top-10 right-2 cursor-pointer"
+            onClick={() => setShow(!show)}
+          >
+            {show ? (
+              <FontAwesomeIcon icon={faEye} color="black" />
+            ) : (
+              <FontAwesomeIcon icon={faEyeSlash} color="black" />
+            )}
+          </div>
+        </div>
+
+        <div className="flex items-center ml-1 mb-2">
+          <input type="checkbox" className="rounded-md size-3 bg-white" />
+          <Link to="/">
+            <div className="ml-1 text-slate-500">Remember me</div>
+          </Link>
+        </div>
+
         <Link to="/signup">
-          <a className="ml-1" href="#">
-            Create a new account!
-          </a>
+          <div className="ml-1 mb-2 text-slate-500">Forgot password</div>
         </Link>
+
+        <div className="border-2 border-slate-400"></div>
+
+        <Link to="/signup">
+          <div className="ml-1 mb-2 mt-2 text-sm">Create a new account!</div>
+        </Link>
+
         <div className="button-group mt-5 flex justify-between items-center mx-5">
           <Link to="/">
             <div className="text-gray-900 text-xl cursor-pointer hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-medium">
